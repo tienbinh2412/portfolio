60e36e79eac003f9fcf7ae6bcc04f17b636ec235
Binh24122000 <binh.dinh@kovena.com>
2025-03-02 11:24:31 +0700

    first commit portfolio

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -47,9 +47,9 @@ const Hero = () => {
           initial="initial"
           animate="animate"
         >
-          <motion.h2 variants={textVariants}>HARVEY TYLER</motion.h2>
+          <motion.h2 variants={textVariants}>NGUYEN KIM NGAN</motion.h2>
           <motion.h1 variants={textVariants}>
-            Web developer and UI designer
+            Content Creator
           </motion.h1>
           <motion.div variants={textVariants} className="buttons">
             <motion.button variants={textVariants}>
@@ -71,7 +71,7 @@ const Hero = () => {
         initial="initial"
         animate="animate"
       >
-        Writer Content Creator Influencer
+        Content Creator
       </motion.div>
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
