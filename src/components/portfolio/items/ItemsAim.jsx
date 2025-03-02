5a8ba10e32e7c7f71900af526f6728ad66a26536
Binh24122000 <binh.dinh@kovena.com>
2025-03-02 18:46:47 +0700

    "upload link image"

--- src/components/portfolio/items/ItemsAim.jsx
+++ src/components/portfolio/items/ItemsAim.jsx
@@ -2,7 +2,7 @@ const itemsAim = [
     {
       id: 1,
       title: "Hotel Royal Hoi An Gallery",
-      img: "../../../../public/project/forza/hoi-an.png",
+      img: "https://nguyen-kim-ngan-portfolio.netlify.app/project/forza/hoi-an.png",
       desc: "Create engaging English content on travel and hospitality.",
       linkOne: "https://hotelroyalhoian.vn/hoi-an-where-to-stay/",
       linkTwo: "https://hotelroyalhoian.vn/travel-guide-to-hoi-an/"
@@ -10,7 +10,7 @@ const itemsAim = [
     {
       id: 2,
       title: "Hafele Home",
-      img: "../../../../public/project/forza/hafale.png",
+      img: "https://nguyen-kim-ngan-portfolio.netlify.app/project/forza/hafale.png",
       desc: "Write articles about interior equipment, build topical maps and achieve 80% of articles with TOS over 1 minute",
       linkOne: "https://hafelehome.com.vn/collections/may-rua-chen-hafele",
       linkTwo: "https://hafelehome.com.vn/blogs/y-tuong-thiet-ke/chuc-nang-booster-cua-bep-tu#:~:text=c%E1%BB%A7a%20b%E1%BA%BFp%20t%E1%BB%AB-,Ch%E1%BB%A9c%20n%C4%83ng%20Booster%20l%C3%A0%20g%C3%AC%3F,n%C3%B3ng%20ch%E1%BB%89%20trong%20v%C3%A0i%20ph%C3%BAt."
@@ -18,7 +18,7 @@ const itemsAim = [
     {
       id: 3,
       title: "Harvey Law Group",
-      img: "../../../../public/project/forza/harvey-law.png",
+      img: "https://nguyen-kim-ngan-portfolio.netlify.app/project/forza/harvey-law.png",
       desc: "Write articles about settlement.",
       linkOne: "https://www.dautudinhcu.com/dinh-cu-anh-co-kho-khong/#:~:text=%C4%90%E1%BB%8Bnh%20c%C6%B0%20Anh%20kh%C3%B4ng%20kh%C3%B3,Anh%20ho%C3%A0n%20to%C3%A0n%20kh%E1%BA%A3%20thi.",
       linkTwo: "https://www.dautudinhcu.com/cuoc-song-o-my/"
@@ -26,7 +26,7 @@ const itemsAim = [
     {
       id: 4,
       title: "Apollo English",
-      img: "../../../../public/project/forza/apolo.png",
+      img: "https://nguyen-kim-ngan-portfolio.netlify.app/project/forza/apolo.png",
       desc: "Write an article on the central topic of English and optimize the TOS of the article in 1 minute.",
       linkOne: "https://apollo.edu.vn/goc-se-chia/sau-danh-tu-la-gi#:~:text=Trong%20ti%E1%BA%BFng%20Anh%2C%20sau%20danh,r%E1%BB%99ng%20%C3%BD%20ngh%C4%A9a%20c%E1%BB%A7a%20c%C3%A2u.",
       linkTwo: "https://apollo.edu.vn/goc-se-chia/hoc-tieng-anh-cho-be-5-tuoi"
@@ -34,7 +34,7 @@ const itemsAim = [
     {
       id: 5,
       title: "Build Group 'Tự Học Semantic SEO'",
-      img: "../../../../public/project/forza/semantic.png",
+      img: "https://nguyen-kim-ngan-portfolio.netlify.app/project/forza/semantic.png",
       desc: "Built a group within 3 months and reached 5,000 members. The highest post reached over 10,000 views and 160 shares.",
       linkOne: "https://www.facebook.com/groups/776038804496779/posts/845284644238861/?__cft__[0]=AZUuW5TPOoXlsgAVKBY_DALgSa4ZWaaqNq9tNVLtqd5nGhGhXHY01dO1zoGcHNCHGeM_2SpZylCguiqfuVswWNfWwHr4AnbO5gBPopDXrMJuk_h2xEd-iuQkL4XJhDcRnJJuSA4JvuPAl15QEPYJd5MJL2AcUrBkd28NYElB0HDQ2w&__tn__=%2CO%2CP-R",
       linkTwo: "https://www.facebook.com/groups/776038804496779/posts/829484119152247/"
